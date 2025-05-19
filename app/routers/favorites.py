d75f7401f17c5057262ff1d0a014b37fc00aa018
yarenbati <yarenbati07@gmail.com>
2025-05-19 14:03:28 +0300

    fırst

---
+++ app/routers/favorites.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from app import schemas, crud
+from app.dependencies import get_db
+from fastapi import APIRouter, Depends
+
+router = APIRouter(prefix="/favorites", tags=["Favorites"])
+
+@router.post("/", response_model=schemas.Favorite)
+def create_favorite(favorite: schemas.FavoriteBase, db: Session = Depends(get_db)):
+    return crud.create_favorite(db, favorite)
+
+@router.get("/", response_model=list[schemas.Favorite])
+def list_favorites(db: Session = Depends(get_db)):
+    return crud.get_favorites(db)
